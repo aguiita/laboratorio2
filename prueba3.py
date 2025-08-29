24d73fdae8cb0ef2d7c23124893c2f1397ddc348
aguiita <145377675+aguiita@users.noreply.github.com>
2025-08-29 14:13:48 -0300

    Update prueba3.py

--- prueba3.py
+++ prueba3.py
@@ -40,6 +40,58 @@ for archivo in archivos:
   #Cada matriz es una “rebanada” del volumen.
 
 #ahora la lista de matrices la hago 3d
-#Eje Z = número de imágenes
-#Eje Y = altura de la imagen
-#Eje X = ancho de la imagen
+#porque quiero tener un array 3d, una pila de mis matrices
+#Eje Z, es el 0 = número de imágenes
+#Eje Y , es el 1 = altura de la imagen
+#Eje X , es el 2 = ancho de la imagen
+volumen = np.stack(imagenes, axis=0)
+#axis 0 porque quiero apilar a lo largo del primer eje (el z)
+# lo convierto en un bloque continuo de datos en memoria
+#el npstack lo que hace es tomar cada matriz de la lista y las apila en un nuevo eje
+#osea si tengo 100 imagenes de 200x200, me queda un array de 100x200x200
+print("Dimensiones del volumen:", volumen.shape)
+
+
+#creo como una grilla de coordenadas de los puntos en 3d
+Z,Y,X= np.indices(volumen.shape) #me devuelve 3 arraays
+x=X.flatten()
+y=Y.flatten()
+z=Z.flatten()
+#flatten lo que hace es convertir la matriz en un vector,lo aplasta
+#osea si tengo una matriz de 200x200, me queda un vector de 40000
+#osea todas las coordenadas de los puntos en 3d
+#ahora tengo 3 vectores, uno para cada coordenada
+#cada vector tiene la misma cantidad de elementos
+#osea si tengo 100 imagenes de 200x200, me queda un vector de 4000000
+#osea todas las coordenadas de los puntos en 3d (100*200*200) 
+                  
+#coords[0].shape   # (2, 3, 4)
+#coords[0].flatten().shape  # (24,), te da una lista con valores, lista lineal.
+
+
+
+#puedo graficar con 
+#ax.scatter(x.flatten(), y.flatten(), z.flatten())
+#donde cada (x[i], y[i], z[i]) es un voxel en el espacio.
+
+
+C = volumen.flatten() #me aplasta el valor del volumen
+
+# puedo usar un ax.scatter(x.flatten(), y.flatten(), z.flatten())
+# donde cada (x[i], y[i], z[i]) es un voxel en el espacio.
+# scatter es para graficar puntos
+# uso un factor para usar solo algunos voxeles y no todos para q no se cuelgue
+
+factor=32
+x=x[::factor ]
+y=y[::factor ]
+z=z[::factor ]
+C=C[::factor ]  
+# me quedaria : [0,1,2,3,4,5,6,7,8,9][::3] → [0,3,6,9]
+
+# ahora con mi array 3d, hago mi figura 3d
+fig = plt.figure(figsize=(10, 8)) # 10 x 8 pulgadas 
+ax=fig.add_subplot(111, projection='3d')
+# 111 significa 1 fila, 1 columna, 1er subplot
+# projection='3d' es para que sea 3d      
+
